02fedecbea2922945cf471bf668fa5a801a82db6
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-12 15:30:36 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,5 +4,7 @@ require 'active_support/inflector'
 class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{{table_name}}')"
   end
 end
