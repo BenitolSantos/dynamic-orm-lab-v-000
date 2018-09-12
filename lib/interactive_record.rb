ffe7b2f7996679a45fd85f769ecb68a8536644b5
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-12 16:57:53 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
     end
 
     def self.find_by(thing)
-      #thing is a hash
+      #thing is a hash, key is symbol so must be converted to text, value is 
       if !(thing.values[0].is_a? Integer)
       sql = "SELECT * FROM #{self.table_name} WHERE #{thing.keys[0].to_s} = '#{thing.values[0]}'"
       #quotes needed for incoming TEXT
